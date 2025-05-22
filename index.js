f66921c18c85763e094ebec74f2408df13437a77
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-22 15:00:45 +0200

    adding console logs index.js

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const store = new tallyStore();
 
 tallyStore.subscribe((state) => console.log(state));
 
+console.log('Dispatching ADD action...');
 tallyStore.dispatch({ type: 'ADD'});
+
+console.log('Dispatching ADD action one more time...');
 tallyStore.dispatch({ type: 'ADD'});
+
+console.log('Dispatching SUBTRACT action...');
 tallyStore.dispatch({ type: 'SUBTRACT'});
+
+console.log('Dispatching RESET action...');
 tallyStore.dispatch({ type: 'RESET'});
