e0a1556685768fac38ee3caeff6ad06fec65ed40
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-23 15:44:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const store = new TallyStore();
 
 store.subscribe((state) => console.log('New state:', state));
 
-console.log('Dispatching ADD action...');
+
 store.dispatch({ type: 'ADD'});
 
-console.log('Dispatching ADD action one more time...');
+
 store.dispatch({ type: 'ADD'});
 
-console.log('Dispatching SUBTRACT action...');
+
 store.dispatch({ type: 'SUBTRACT'});
 
-console.log('Dispatching RESET action...');
+
 store.dispatch({ type: 'RESET'});
