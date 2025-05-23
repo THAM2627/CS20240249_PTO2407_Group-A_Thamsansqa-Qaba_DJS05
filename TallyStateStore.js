b7ee965cdcda3d8f68872e57788161c2a25700b0
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-23 11:45:47 +0200

    console.log updates

--- TallyStateStore.js
+++ TallyStateStore.js
@@ -12,14 +12,15 @@ class TallyStore {
         switch (action.type){
             case 'ADD' :
                 this.tally.count += 1;
-           
+                console.log('After ADD:', this.tally.count);
                 break;
             case 'SUBTRACT' :
                 this.tally.count -= 1;
-           
+                console.log('After SUBTRACT:', this.tally.count);
                 break;
             case 'RESET' :
                 this.tally.count = 0;
+                console.log('After RESET:', this.tally.count);
                 break;
 
                 default:
